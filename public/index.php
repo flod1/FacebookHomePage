9ebf11e995890171b2cb7c6dfd44f6991ae7782e
Florian Degenhardt <florian.degenhardt@studiointeractive.de>
2016-09-14 08:07:27 +0200

    Refactoring

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+
+/**
+ * Display all errors when APPLICATION_ENV is development.
+ */
+
+// Define application environment
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+//development Environment
+if (APPLICATION_ENV == 'development') {
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED & ~E_STRICT);
+    ini_set("display_errors", 1);
+} else {
+    error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED & ~E_STRICT);
+
+}
+
+chdir(dirname(__DIR__));
+
+// Decline static file requests back to the PHP built-in webserver
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
