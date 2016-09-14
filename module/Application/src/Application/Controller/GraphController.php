9bb7832cbf6190c1d2cf21eb27b18ccfa2f89294
Florian Degenhardt <florian.degenhardt@studiointeractive.de>
2016-09-14 15:00:32 +0200

    Refactoring

---
+++ module/Application/src/Application/Controller/GraphController.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\View\View;
+
+class GraphController extends AbstractActionController
+{
+    /**
+     * @var \FbBasic\Service\FacebookBase
+     */
+    protected $facebookbaseService;
+
+    /**
+     * Getters/setters for DI stuff
+     */
+
+    /**
+     * @return \FbBasic\Service\FacebookBase
+     */
+    public function getFacebookBaseService()
+    {
+        if (!$this->facebookbaseService) {
+            $this->facebookbaseService = $this->serviceLocator->get('fbbasic_graph_service');
+        }
+        return $this->facebookbaseService;
+    }
+
+    public function setFacebookBaseService(\FbBasic\Service\FacebookBase $facebookbaseService)
+    {
+        $this->facebookbaseService = $facebookbaseService;
+        return $this;
+    }
+
+    public function nodeAction()
+    {
+
+        $viewHelperManager = $this->getServiceLocator()->get('ViewHelperManager');
+        $pageWidget = $viewHelperManager->get('pageWidget');
+        //$graphWidget = $viewHelperManager->get('graphWidget');
+
+        /**
+         * @var $pageWidget \FbPage\View\Helper\PageWidget
+         * @var $graphWidget \FbBasic\View\Helper\GraphWidget
+         */
+
+
+        $id = $this->params()->fromRoute("id");
+        $parameters = array("metadata" => 1);
+        $response = $this->getFacebookBaseService()->fetchGraphNode($id, $parameters);
+        $graphNode = $response->getGraphNode();
+
+        /** @var $metadata \Facebook\GraphNodes\GraphNode */
+        $metadata = $graphNode->getField("metadata");
+        $nodetype = $metadata->getField("type");
+
+        $graphEdges = null;
+        switch ($nodetype) {
+            case "page":
+                $graphNode = $this->getFacebookBaseService()->fetchPage($id, "*");
+                $pageWidget->setPageID($id);
+
+                $graphEdge = array();
+                $graphEdge['key'] = "albums";
+                $graphEdge['count'] = $pageWidget->fetchAllAlbums()->count();
+                $graphEdges[] = $graphEdge;
+
+                $graphEdge = array();
+                $graphEdge['key'] = "events";
+                $graphEdge['count'] = $pageWidget->fetchAllEvents()->count();
+                $graphEdges[] = $graphEdge;
+
+                $graphEdge = array();
+                $graphEdge['key'] = "milestones";
+                $graphEdge['count'] = $pageWidget->fetchAllMilestones()->count();
+                $graphEdges[] = $graphEdge;
+
+                $graphEdge = array();
+                $graphEdge['key'] = "posts";
+                $graphEdge['count'] = $pageWidget->fetchAllPosts()->count();
+                $graphEdges[] = $graphEdge;
+                break;
+            case "photo":
+                $graphNode = $this->getFacebookBaseService()->fetchPhoto($id, "*");
+                break;
+            case "album":
+                $graphNode = $this->getFacebookBaseService()->fetchAlbum($id, "*");
+
+                $graphEdge = array();
+                $graphEdge['key'] = "photos";
+                $graphEdge['count'] = $this->getFacebookBaseService()->fetchPhotosByAlbum($id,"*")->count();
+                $graphEdges[] = $graphEdge;
+                break;
+            case "event":
+                $graphNode = $this->getFacebookBaseService()->fetchEvent($id, "*");
+                break;
+            default : var_dump($nodetype);
+
+        }
+
+        $graphNodeType = ucfirst($nodetype);
+
+
+        var_dump($this->params()->fromRoute());
+        return new ViewModel(array("graphNode" => $graphNode, "graphEdges" => $graphEdges, "graphNodeType" => $graphNodeType));
+
+    }
+
+    public function edgeAction()
+    {
+
+        $viewHelperManager = $this->getServiceLocator()->get('ViewHelperManager');
+        $pageWidget = $viewHelperManager->get('pageWidget');
+        //$graphWidget = $viewHelperManager->get('graphWidget');
+
+        /**
+         * @var $pageWidget \FbPage\View\Helper\PageWidget
+         * @var $graphWidget \FbBasic\View\Helper\GraphWidget
+         */
+
+
+        $id = $this->params()->fromRoute("id");
+        $parameters = array("metadata" => 1);
+        $response = $this->getFacebookBaseService()->fetchGraphNode($id, $parameters);
+        $graphNode = $response->getGraphNode();
+
+        /** @var $metadata \Facebook\GraphNodes\GraphNode */
+        $metadata = $graphNode->getField("metadata");
+        $nodetype = $metadata->getField("type");
+
+        $graphEdges = null;
+        switch ($nodetype) {
+            case "page":
+                $graphNode = $this->getFacebookBaseService()->fetchPage($id, "*");
+                $pageWidget->setPageID($id);
+
+                $graphEdge = array();
+                $graphEdge['key'] = "albums";
+                $graphEdge['count'] = $pageWidget->fetchAllAlbums()->count();
+                $graphEdges[] = $graphEdge;
+
+                $graphEdge = array();
+                $graphEdge['key'] = "events";
+                $graphEdge['count'] = $pageWidget->fetchAllEvents()->count();
+                $graphEdges[] = $graphEdge;
+
+                $graphEdge = array();
+                $graphEdge['key'] = "milestones";
+                $graphEdge['count'] = $pageWidget->fetchAllMilestones()->count();
+                $graphEdges[] = $graphEdge;
+
+                $graphEdge = array();
+                $graphEdge['key'] = "posts";
+                $graphEdge['count'] = $pageWidget->fetchAllPosts()->count();
+                $graphEdges[] = $graphEdge;
+                break;
+            case "photo2":
+                $graphNode = $this->getFacebookBaseService()->fetchEvent($id, "*");
+                break;
+            case "event":
+                $graphNode = $this->getFacebookBaseService()->fetchEvent($id, "*");
+                break;
+            default : var_dump($nodetype);
+
+        }
+
+        $edge = $this->params()->fromRoute("edge");
+
+        $subclass = null;
+        switch ($edge) {
+            case "events":
+                $graphEdge = $this->getFacebookBaseService()->fetchEvents($id, "*");
+                break;
+            case "albums":
+                $graphEdge = $this->getFacebookBaseService()->fetchAlbums($id, "*");
+                break;
+            case "photos":
+                $graphEdge = $this->getFacebookBaseService()->fetchPhotosByAlbum($id, "*");
+                break;
+            //case "photos":$subclass = "GraphPicture";break;
+            default :
+                $graphEdge = $this->getFacebookBaseService()->fetchGraphEdge($id, $edge, $subclass, $parameters);
+        }
+
+        $graphNodeType = ucfirst($nodetype);
+        $graphEdgeType = ucfirst($edge);
+
+        return new ViewModel(array("graphNode" => $graphNode, "graphEdge" => $graphEdge, "graphNodeType" => $graphNodeType, "graphEdgeType" => $graphEdgeType));
+
+    }
+
+}
+
